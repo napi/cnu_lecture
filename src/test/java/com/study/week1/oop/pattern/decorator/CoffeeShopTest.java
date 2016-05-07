174ac63a4b74c1857dfa192c06ad1c706deb5fa6
rokim <rokim@riotgames.com>
2016-05-07 22:58:58 +0900

    Add generic example

--- src/test/java/com/study/week1/oop/pattern/decorator/CoffeeShopTest.java
+++ src/test/java/com/study/week1/oop/pattern/decorator/CoffeeShopTest.java
@@ -13,7 +13,7 @@ public class CoffeeShopTest {
 		CoffeeShop coffeeShop = new CoffeeShop();
 		Beverage beverage = coffeeShop.getMochaCoffee();
 				
-		assertThat(beverage.cost(), is(0));
+		assertThat("모카커피 가격은 3000원 이다", beverage.cost(), is(0));
 	}
 	
 	@Test
@@ -21,7 +21,7 @@ public class CoffeeShopTest {
 		CoffeeShop coffeeShop = new CoffeeShop();
 		Beverage beverage = coffeeShop.getIceMochaCoffee();
 				
-		assertThat(beverage.cost(), is(0));
+		assertThat("아이스 모카 커피 가격은 3500원 이다.", beverage.cost(), is(0));
 	}
 
 	// TODO 크림 커피를 만들어 봅시다. 크림은 저렴하게 100원으로.
@@ -30,13 +30,15 @@ public class CoffeeShopTest {
 		CoffeeShop coffeeShop = new CoffeeShop();
 		Beverage beverage = coffeeShop.getIceCreamMochaCoffee();
 		
-		assertThat(beverage.cost(), is(3600));
+		assertThat("아이스 크림 모카커피는 3600원 이다.", beverage.cost(), is(3600));
 	}
 	
+	// TODO 에스프레소의 가격이 2500원으로 인상됐다.
+	// EspressoBeverage 의 cost 를 2500원 으로 올리고 테스트 코드를 수정하라.
+	
+	
+	
 	// TODO Decorator가 유연하지 않아요. cost 를 변경할 수는 없나요?
-	// 가능하다. cost 를 Decorator 의 field 로 둠으로서 한단계 더 추상화가 가
-
-	
-	// TODO 저렴하고 맛있는 충남대 커피샵을 열어보아요
-	
+	// 가능하다. cost 를 Decorator 의 field 로 둠으로서 한단계 더 추상화가 가능하며, Decorator를 interface 로 바꾸는 것도 좋은 방법이다.
+	// 저렴하고 맛있는 충남대 커피샵을 열어보아요
 }
--- src/test/java/com/study/week1/quiz/EqualQuizTest.java
+++ src/test/java/com/study/week1/quiz/EqualQuizTest.java
@@ -17,7 +17,8 @@ public class EqualQuizTest {
 		
 		Integer n1 = new Integer(100);
 		Integer n2 = new Integer(100);		
-		assertThat("래퍼러 비교", n1 == n2, is(true));
+		// matcher 'is(true)' 를 유지한 체로 테스트 코드 수정.
+		assertThat("래퍼러의 == 비교", n1 == n2, is(true));		
 	}
 	
 	@Test
@@ -25,13 +26,14 @@ public class EqualQuizTest {
 		EqualQuiz q1 = new EqualQuiz();
 		EqualQuiz q2 = new EqualQuiz();
 		q1.i = q2.i = 100;
-						
+
+		// TODO 설명 듣고, 테스트가 동작하도록 EqualQuiz.java 파일을 수정.
 		assertThat("객체 비교", q1.equals(q2), is(true));
 		
+		
+		// Object 객체의 상속 구조와 equals 메서드의 Override에 대해 설명.
 		Object obj = new Object();
 		Integer integer = new Integer(0);
-
-		// TODO 설명 듣고, 테스트가 동작하도록 고쳐보아요.
 	}
 	
 	@Test
@@ -40,16 +42,27 @@ public class EqualQuizTest {
 		String s2 = "Hello";
 		
 		assertThat("String equals", s1.equals(s2), is(true));
-		assertThat("String 비교 s1 == s2", s1 == s2, is(false));
-		
-		String s3 = "He" + "llo";
-		assertThat("String 비교 s1 == s3", s1 == s3, is(false));
-		
-		String s4 = "He";
-		String s5 = "llo";
-		assertThat("String 비교4 s1 == s4 + s5", s1 == s4 + s5, is(false));
-		
-		assertThat("String 비교5 s1 equals (s4+s5)", s1.equals(s4 + s5), is(true));
+		assertThat("String 은 객체인데 s1 == s2가 true 라니!?", s1 == s2, is(false));
 	}
-
+	
+	@Test
+	public void testStringConcat() throws Exception {
+		String s1 = "Hello";
+		String s2 = "He" + "llo";
+		
+		assertThat("Hello == 'He' + 'llo' => true", s1.equals(s2), is(true));
+		assertThat("Hello == 'He' + 'llo' => true", s1 == s2, is(true));
+	}
+	
+	@Test
+	public void testStringConcat2() throws Exception {
+		String s1 = "Hello";
+		String s2 = "He";
+		String s3 = "llo";
+		
+		String s4 = s2 + s3;
+		
+		assertThat("Hello == 'He' + 'llo' => false", s1.equals(s4), is(true));
+		assertThat("Hello == 'He' + 'llo' => false", s1 == s4, is(true));
+	}
 }
