a6d799cd3fdeb687516db73c5e0da6bf5d8deceb
rokim <rokim@riotgames.com>
2016-05-09 01:36:21 +0900

    Fix something to easy

---
+++ src/main/java/com/study/common/PageInfo.java
@@ -0,0 +1,11 @@
+package com.study.common;
+
+public class PageInfo {
+	private int currentPage;
+	private int pageSize;
+	private int rowSize;
+	
+	public PageInfo(int currentPage, int pageSize, int rowSize) {
+		
+	}
+}
