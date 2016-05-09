acca306dd9e5fb140526241dfc80d8d21a0e54a2
rokim <rokim@riotgames.com>
2016-05-09 19:47:12 +0900

    Fix strategy homework

--- src/main/java/com/study/week1/oop/pattern/strategy/Player.java
+++ src/main/java/com/study/week1/oop/pattern/strategy/Player.java
@@ -9,20 +9,25 @@ public class Player {
 	private String name;
 	private WeaponStrategy weaponStrategy;
 	
-	public Player(WeaponStrategy weaponStrategy) {
-		this.weaponStrategy = weaponStrategy;
-	}
-	
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
+	public Player(String name) {
 		this.name = name;
 	}
 	
+	public void setWeapon(WeaponStrategy strategy) {
+		this.weaponStrategy = strategy;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
 	public int attack() {
-		log.info("[{}] 가 공격을 시작합니다");
+		if (weaponStrategy == null) {
+			log.info("[{}] 가 맨손으로 공격하여 {}의 데미지를 가합니다.", name, 1);
+		} else {
+			log.info("[{}] 가 공격하여 {}의 데미지를 가합니다.", name, this.weaponStrategy.damage());	
+		}
+		
 		return this.weaponStrategy.damage();
 	}
 
