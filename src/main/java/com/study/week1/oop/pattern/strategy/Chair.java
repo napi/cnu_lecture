a39ea081f98fdd10dc64d6458f574f88c8eca97a
sh92 <sanghee.lee1992@gmail.com>
2016-05-11 01:16:02 +0900

    Implements WaeponStrategy in Chair.java
    WeaponStrategy를 구현하여 damage메서드를 override를 하여 구현하여
    기본데미지를 2로 하고 크리스탈재료일 경우 100으로 한다. 그리고  Chair의 경우
    WeaponStrategy를 구현하였으므로 player가 무기를 설정할 때 다형성에 의해
    WeaponStrategy 타입으로 정의된 함수에서Chair 타입의 객체를 받을 수 있다.

--- src/main/java/com/study/week1/oop/pattern/strategy/Chair.java
+++ src/main/java/com/study/week1/oop/pattern/strategy/Chair.java
@@ -3,19 +3,19 @@ package com.study.week1.oop.pattern.strategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Chair {
+public class Chair implements WeaponStrategy{
 	Logger log = LoggerFactory.getLogger(this.getClass());
 	private Material material;
-	
-	
+
+
 	public Chair(Material material) {
 		this.material = material;
 	}
-	
+
 	public void setMaterial(Material material) {
 		this.material = material;
 	}
-	
+
 	public void sit(Player player) {
 		log.info("[{}] 님이 의자에 앉아 휴식을 취합니다", player.getName());
 	}
@@ -26,4 +26,15 @@ public class Chair {
 		IRON,
 		CRISTAL
 	}
+
+	@Override
+	public int damage() {
+		switch(material){
+			case CRISTAL:
+				return 100;
+			default:
+				return 2;
+		}
+	}
+
 }
