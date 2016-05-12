35ea89bb92e4d9fa35cf12a0119eeb934ea4abd3
MS-Park <deceglie92@gmail.com>
2016-05-13 01:14:45 +0900

    Update homework

--- src/main/java/com/study/week1/quiz/EqualQuiz.java
+++ src/main/java/com/study/week1/quiz/EqualQuiz.java
@@ -2,4 +2,14 @@ package com.study.week1.quiz;
 
 public class EqualQuiz {
 	public int i;
+	
+	public boolean equals(Object obj) {
+		if (obj instanceof EqualQuiz) {
+			EqualQuiz quiz = (EqualQuiz)obj;
+			if(this.i == quiz.i){
+				return true;
+			}
+		}
+		return false;
+	}
 }
