c8774518ee3dfbc10e46ed71530a403fdbdfb447
HyungJiny <bbqnn200@gmail.com>
2016-05-13 18:43:58 +0900

    AccessParentTest에서 발생하는 테스트 에러 제거

--- src/test/java/com/study/week1/oop/access/AccessParentTest.java
+++ src/test/java/com/study/week1/oop/access/AccessParentTest.java
@@ -28,11 +28,13 @@ public class AccessParentTest {
 	 */
 	@Test
 	public void another() {
+	    // "Another : " 를 추가하여 문제 해결
 		assertThat("Test코드를 수정해서 아래 코드를 해결할 것", another.getAccessParentNameByDefault(), is("Another : AccessParent_Default"));
 	}
 	
 	@Test
 	public void testAccessNameByProtected() {
+	    // "Child : "를 추가하여 문제 해결
 		assertThat("Test코드를 수정해서 아래 코드를 해결할 것", accessChildren.getNameByProtected(), is("Child : AccessParent_Protected"));
 	}
 
@@ -45,6 +47,7 @@ public class AccessParentTest {
 //		accessChildrenCasted.onlyChildren();	compile error
 		
 		// TODO Test코드를 수정해서 아래 fail을 해결
+		// "Child : "를 추가하여 문제 해결
 		assertThat("parent 로 캐스팅 됐지만, 객체는 여전히 children의 속성을 갖고 있다.", 
 				accessParentCasted.getNameByProtected(), is("Child : AccessParent_Protected")); // 나 부모로 캐스팅 된거 아닌가??
 	}
@@ -59,9 +62,11 @@ public class AccessParentTest {
 		float f2 = i;
 		
 		// TODO Test코드를 수정해서 아래 fail을 해결
+		// 뒤의 소수점 데이터가 사라져 123->123.0f 로 변경하여 문제 해결
 		assertThat("데이터가 유실됐다!! primary type은 캐스팅때 조심해야 함.", f2, is(123.0f));		
 	}
 	
+	// expected를 이용하여 debug에서 나오는 exception class 추가하여 문제 해결
 	@Test(expected=ClassCastException.class)
 	public void testException() throws Exception {
 		/* Compile error  
