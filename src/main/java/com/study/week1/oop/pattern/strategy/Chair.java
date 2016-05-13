f1d671732d40d5dcc1358e6d1b4d45aa46ef1b3d
seunghee2 <smilagain07@daum.net>
2016-05-13 23:19:51 +0900

    test_success_final

--- src/main/java/com/study/week1/oop/pattern/strategy/Chair.java
+++ src/main/java/com/study/week1/oop/pattern/strategy/Chair.java
@@ -3,27 +3,37 @@ package com.study.week1.oop.pattern.strategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Chair {
+public class Chair implements WeaponStrategy {
 	Logger log = LoggerFactory.getLogger(this.getClass());
 	private Material material;
-	
-	
+
 	public Chair(Material material) {
 		this.material = material;
 	}
-	
+
 	public void setMaterial(Material material) {
 		this.material = material;
 	}
-	
+
 	public void sit(Player player) {
 		log.info("[{}] 님이 의자에 앉아 휴식을 취합니다", player.getName());
 	}
 
 	enum Material {
-		WOOD,
-		STONE,
-		IRON,
-		CRISTAL
+		WOOD, STONE, IRON, CRISTAL
+	}
+
+	@Override
+	public int damage() {
+		switch (this.material) {
+		case WOOD:
+		case STONE:
+		case IRON:
+			return 2;
+		case CRISTAL:
+			return 100;
+		default:
+			return 0;
+		}
 	}
 }
