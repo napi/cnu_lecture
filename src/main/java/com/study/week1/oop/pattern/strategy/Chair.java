9d69fec88060ae575318a7e2925b2e8264467433
Yoonjae <yoonjae.cho92@gmail.com>
2016-05-13 16:45:34 +0900

    Fix test code to run

--- src/main/java/com/study/week1/oop/pattern/strategy/Chair.java
+++ src/main/java/com/study/week1/oop/pattern/strategy/Chair.java
@@ -3,7 +3,7 @@ package com.study.week1.oop.pattern.strategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Chair {
+public class Chair implements WeaponStrategy {
 	Logger log = LoggerFactory.getLogger(this.getClass());
 	private Material material;
 	
@@ -26,4 +26,20 @@ public class Chair {
 		IRON,
 		CRISTAL
 	}
+
+    @Override
+    public int damage() {
+        
+        if(this.material == Material.WOOD) {
+            return 2;
+        } else if(this.material == Material.STONE) {
+            return 4;
+        } else if(this.material == Material.IRON) {
+            return 10;
+        } else if(this.material == Material.CRISTAL) {
+            return 100;
+        } else {
+            return 2;
+        }
+    }
 }
