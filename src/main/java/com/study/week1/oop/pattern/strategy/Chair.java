1168066d714d45d4d3298857ba66136e9eb50520
starlightcandy <uta9784@gmail.com>
2016-05-14 17:55:30 +0900

    Implements WeaponStrategy, Override damage function to pass the test.

--- src/main/java/com/study/week1/oop/pattern/strategy/Chair.java
+++ src/main/java/com/study/week1/oop/pattern/strategy/Chair.java
@@ -3,27 +3,44 @@ package com.study.week1.oop.pattern.strategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Chair {
-	Logger log = LoggerFactory.getLogger(this.getClass());
-	private Material material;
-	
-	
-	public Chair(Material material) {
-		this.material = material;
-	}
-	
-	public void setMaterial(Material material) {
-		this.material = material;
-	}
-	
-	public void sit(Player player) {
-		log.info("[{}] 님이 의자에 앉아 휴식을 취합니다", player.getName());
-	}
+public class Chair implements WeaponStrategy {
+    Logger log = LoggerFactory.getLogger(this.getClass());
+    private Material material;
 
-	enum Material {
-		WOOD,
-		STONE,
-		IRON,
-		CRISTAL
-	}
+
+    public Chair(Material material) {
+        this.material = material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public void sit(Player player) {
+        log.info("[{}] 님이 의자에 앉아 휴식을 취합니다", player.getName());
+    }
+
+    enum Material {
+        WOOD,
+        STONE,
+        IRON,
+        CRISTAL
+    }
+
+    @Override
+    public int damage() {
+
+        if (this.material == Material.WOOD) {
+            return 2;
+        } else if (this.material == Material.STONE) {
+            return 4;
+        } else if (this.material == Material.IRON) {
+            return 10;
+        } else if (this.material == Material.CRISTAL) {
+            return 100;
+        } else {
+            return 2;
+        }
+    }
+
 }
