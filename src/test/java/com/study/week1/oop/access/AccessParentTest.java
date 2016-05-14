0e45a7ca20ca548549719cf8c3d1d7f12c8c105e
starlightcandy <uta9784@gmail.com>
2016-05-14 17:43:32 +0900

    Modified code to pass tests

--- src/test/java/com/study/week1/oop/access/AccessParentTest.java
+++ src/test/java/com/study/week1/oop/access/AccessParentTest.java
@@ -13,14 +13,14 @@ public class AccessParentTest {
 	private AccessParent accessParent;
 	private Another another;
 	private AccessChildren accessChildren;
-	
+
 	@Before
 	public void init() {
 		accessParent = new AccessParent();
 		another = new Another(accessParent);
 		accessChildren = new AccessChildren();
 	}
-	
+
 	
 	/**
 	 * Another 클래스는 field로 AccessParent 클래스를 갖고 있다.
@@ -28,12 +28,12 @@ public class AccessParentTest {
 	 */
 	@Test
 	public void another() {
-		assertThat("Test코드를 수정해서 아래 코드를 해결할 것", another.getAccessParentNameByDefault(), is("AccessParent_Default"));
+		assertThat("Test코드를 수정해서 아래 코드를 해결할 것", another.getAccessParentNameByDefault(), is("Another : AccessParent_Default"));
 	}
 	
 	@Test
 	public void testAccessNameByProtected() {
-		assertThat("Test코드를 수정해서 아래 코드를 해결할 것", accessChildren.getNameByProtected(), is("AccessParent_Protected"));
+		assertThat("Test코드를 수정해서 아래 코드를 해결할 것", accessChildren.getNameByProtected(), is("Child : AccessParent_Protected"));
 	}
 
 	@Test
@@ -46,15 +46,15 @@ public class AccessParentTest {
 		
 		// TODO Test코드를 수정해서 아래 fail을 해결
 		assertThat("parent 로 캐스팅 됐지만, 객체는 여전히 children의 속성을 갖고 있다.", 
-				accessParentCasted.getNameByProtected(), is("AccessParent_Protected")); // 나 부모로 캐스팅 된거 아닌가??
+				accessParentCasted.getNameByProtected(), is("Child : AccessParent_Protected")); // 나 부모로 캐스팅 된거 아닌가??
 	}
 	
 	@Test
 	public void 랩퍼러_형변환() throws Exception {
 		float f = 123.4f;
-		int i = (int) f;
+		float i = f;
 		
-		assertThat("정상", i, is(123));
+		assertThat("정상", (int)i, is(123));
 
 		float f2 = i;
 
@@ -62,7 +62,7 @@ public class AccessParentTest {
 		assertThat("데이터가 유실됐다!! primary type은 캐스팅때 조심해야 함.", f2, is(123.4f));		
 	}
 	
-	@Test
+	@Test(expected=ClassCastException.class)
 	public void testException() throws Exception {
 		/* Compile error  
 		 * 
@@ -70,7 +70,7 @@ public class AccessParentTest {
 		 * AccessParent parent = (AccessParent)another;	// compile error
 		 * 
 		 * */
-		
+
 		Object obj = (Object)another;
 		AccessParent parent = (AccessParent)obj;
 		
