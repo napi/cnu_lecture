d1738f2a815f133b112af1017fb151bae4965982
rokim <rokim@riotgames.com>
2016-05-15 03:45:05 +0900

    Add CnuBoardController

--- src/main/java/com/study/controller/CnuBoardController.java
+++ src/main/java/com/study/controller/CnuBoardController.java
@@ -1,7 +1,17 @@
 package com.study.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by rokim on 2016. 5. 15..
  */
+@Controller
+@RequestMapping("/board")
 public class CnuBoardController {
+    @RequestMapping("")
+    public String indexl() {
+        return "board/index";
+    }
+
 }
