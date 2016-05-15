33019c8fcf4138f498d729753387dace356439ef
rokim <rokim@riotgames.com>
2016-05-16 03:20:01 +0900

    Add tutorial controller

--- src/main/java/com/study/controller/tutorial/TutorialController.java
+++ src/main/java/com/study/controller/tutorial/TutorialController.java
@@ -4,12 +4,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 /**
  * Created by rokim on 2016. 5. 15..
  */
-// TODO Controller 넣기
-// TODO RequestMapping 넣기
+@Controller
+@RequestMapping("/tutorial")
 public class TutorialController {
-    // TODO return "tutorial/step1"
+
+    @RequestMapping("/step1")
+    public ModelAndView step1(@RequestParam String name) {
+        ModelAndView mav = new ModelAndView();
+
+        Map<String, Object> model = mav.getModel();
+        model.put("name", name);
+
+        mav.setViewName("/tutorial/step1");
+
+        return mav;
+    }
+
+    @RequestMapping("/step2")
+    public ModelAndView step2(@RequestParam String name, Model model) {
+        ModelAndView mav = new ModelAndView();
+
+        model.addAttribute("name", name);
+
+        mav.setViewName("/tutorial/step1");
+
+        return mav;
+    }
+
+    @RequestMapping("/step3")
+    public String step3(@RequestParam String name, @RequestParam String[] names, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("nameList", names);
+
+        return "/tutorial/step1";
+    }
+
 }
