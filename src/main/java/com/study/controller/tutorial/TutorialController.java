8a4eab41258e57b93cae22bd5d3904e1b2f66bf1
rokim <rokim@riotgames.com>
2016-05-16 13:23:42 +0900

    Revert TutorialController

--- src/main/java/com/study/controller/tutorial/TutorialController.java
+++ src/main/java/com/study/controller/tutorial/TutorialController.java
@@ -11,39 +11,5 @@ import java.util.Map;
 /**
  * Created by rokim on 2016. 5. 15..
  */
-@Controller
-@RequestMapping("/tutorial")
 public class TutorialController {
-
-    @RequestMapping("/step1")
-    public ModelAndView step1(@RequestParam String name) {
-        ModelAndView mav = new ModelAndView();
-
-        Map<String, Object> model = mav.getModel();
-        model.put("name", name);
-
-        mav.setViewName("/tutorial/step1");
-
-        return mav;
-    }
-
-    @RequestMapping("/step2")
-    public ModelAndView step2(@RequestParam String name, Model model) {
-        ModelAndView mav = new ModelAndView();
-
-        model.addAttribute("name", name);
-
-        mav.setViewName("/tutorial/step1");
-
-        return mav;
-    }
-
-    @RequestMapping("/step3")
-    public String step3(@RequestParam String name, @RequestParam String[] names, Model model) {
-        model.addAttribute("name", name);
-        model.addAttribute("nameList", names);
-
-        return "/tutorial/step1";
-    }
-
 }
