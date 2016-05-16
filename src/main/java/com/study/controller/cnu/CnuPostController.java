541348510750dcd50426208e05c6ee972c00f09e
rokim <rokim@riotgames.com>
2016-05-16 19:13:05 +0900

    Teach week2

--- src/main/java/com/study/controller/cnu/CnuPostController.java
+++ src/main/java/com/study/controller/cnu/CnuPostController.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 /**
@@ -21,18 +19,49 @@ import java.util.List;
 public class CnuPostController {
     @Value("${application.security.salt}") private String securityKey;
 
+    @Autowired
+    CnuRepository cnuRepository;
+
     @RequestMapping("")
-    public String index() {
+    public String index(Model model) {
+        List<CnuPost> cnuPostList = cnuRepository.selectCnuPostList();
+
+        model.addAttribute("cnuPostList", cnuPostList);
         return "post/index";
     }
 
-    @RequestMapping("/write")
+    @RequestMapping(value = "/write", method = RequestMethod.GET)
     public String write() {
         return "post/write";
+    }
+
+    @RequestMapping(value = "/write", method = RequestMethod.POST)
+    public String doWrite(String title,
+                          String content,
+                          String author,
+                          String password) {
+
+        CnuPost cnuPost = new CnuPost();
+        cnuPost.setTitle(title);
+        cnuPost.setAuthor(author);
+        cnuPost.setPassword(password);
+        cnuPost.setContent(content);
+
+        cnuRepository.insertCnuPost(cnuPost);
+
+        return "redirect:/post";
     }
 
     @RequestMapping("/view")
     public String view() {
         return "post/view";
     }
+
+    @RequestMapping("/delete")
+    public String delete(int postId, String password) {
+
+
+        return "post/view";
+    }
+
 }
