c98a030c30e502a68d2a09e98fb9be187a49ef82
hyeonwoo <chw1787@naver.com>
2016-05-21 00:12:58 +0900

    Modify code in CnuRepository.java

--- src/main/java/com/study/repository/mybatis/CnuRepository.java
+++ src/main/java/com/study/repository/mybatis/CnuRepository.java
@@ -2,20 +2,17 @@ package com.study.repository.mybatis;
 
 import com.study.domain.cnu.CnuPost;
 import com.study.domain.jpa.JpaBoard;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 public interface CnuRepository {
 	List<CnuPost> selectCnuPostList();
-
-
-
-
 	int insertCnuPost(CnuPost obj);
 
-
-	CnuPost selectCnuBoard(int postId);
-	int updateCnuBoard(CnuPost obj);
-	int deleteCnuBoard(int postId, String password);
+	CnuPost selectCnuPost(int postId);
+	int updateCnuPost(CnuPost obj);
+	int deleteCnuPost(int postId, String password);
 }
