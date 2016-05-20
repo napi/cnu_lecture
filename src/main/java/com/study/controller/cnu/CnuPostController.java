f0a145f09e1d298ab841747b3572158a4ed311ff
Parkgeonhee <jilkj1234@naver.com>
2016-05-20 17:38:13 +0900

    6조 comment 수정했습니다.

--- src/main/java/com/study/controller/cnu/CnuPostController.java
+++ src/main/java/com/study/controller/cnu/CnuPostController.java
@@ -58,17 +58,18 @@ public class CnuPostController {
     /*
      * comment 작성 메소드 추가
      */
-    @RequestMapping(value = "/view", method = RequestMethod.POST)
-    public String comment(String author, String password, String comment) {
+    @RequestMapping(value = "/post/view?postId=", method = RequestMethod.POST)
+    public String comment(@RequestParam int postId, String author, String password, String comment) {
     	CnuPostComment cnupostcomment = new CnuPostComment();
     	cnupostcomment.setAuthor(author);
     	cnupostcomment.setPassword(password);
     	cnupostcomment.setComment(comment);
     	cnupostcomment.setCreateTime(new Date());
+    	cnupostcomment.setPostId(postId);
     	
     	cnuRepository.insertCnuPostComment(cnupostcomment);
 		
-    	return "redirect:/post/view" + cnupostcomment.getPostId();
+    	return "redirect:/post/view";
     }
 
     @RequestMapping("/view")
